12118590a990bb8798c616620c94588a9d9c7815
CodeByTurkan <turkantech@gmail.com>
2025-04-10 22:50:36 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+let arr = []
+const baseURL = 'https://67f804312466325443eb919f.mockapi.io/todo' 
+
+// function getList() {
+//     fetch(URL),{
+//         method:'GET',
+//         headers:{
+//             'content-type': 'application/json'
+//         },
+//     }
+//     .then(res => res.json())
+//     .then(data => showlist(data))
+//     .catch(err => console.log('Error catching data', err))
+// }
+// getList()
+
+async function getList() {
+    const response = await fetch(baseURL)
+    const data = await response.json()
+    showList(data)
+}
+getList()
+
+const list = document.querySelector('ul')
+const text = document.getElementById('text') 
+
+function showList(data) {
+    let code = ''
+    data.map(item => {
+        code +=
+        `
+        <li>
+            <div class="mx-auto border-b border-gray-300 p-3 flex justify-between">
+                <span>${item.text}</span>
+                <div>
+                    <button onclick=editTask(${item.id}, '${item.text}')  type="submit" class="text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 ">EDIT</button>
+                    <button onclick = removeTask(${item.id}) type="submit" class="text-white bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 ">DELETE</button>
+                </div>
+            </div>
+        </li>
+        ` 
+    })
+    list.innerHTML = code
+}
+
+// my logical hottakes
+// goruntunu bir defe burda yazirsan mentiq olaraq, ve sonrada elave et sil ve edit eti yazirsan.
+
+function addTask(){
+    if (text.value != '') {
+        fetch(baseURL, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: text.value
+            })
+        })
+            .then(res => {
+                if (res.ok) {
+                    // eger server uguarla qebul edibse onda we upload we new data
+                    getList()
+                }
+            })
+    }else {
+        alert('Please, fill the input!')
+    }
+    text.value = ''
+}
+
+function removeTask(id) {
+    removeBtn.closest('li').remove()
+}
+function editTask(id, text) {
+   let editItem = editBtn.closest('li')
+   let previousText = editItem.querySelector("span").textContent
+   
+   editItem.innerHTML = 
+    `<li>
+        <div class="mx-auto border-b border-gray-300 p-3 flex justify-between">
+           <input class="p-2 border rounded" type="text" name="" id="" value="${previousText}">
+            <div>
+                <button onclick =saveTask(this)  type="submit" class="text-white bg-green-600 font-medium rounded-lg text-sm px-4 py-2 ">SAVE</button>
+                <button onclick = cancelText(this) type="submit" class="text-white bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 ">CANCEL</button>
+            </div>
+        </div>
+    </li>`
+}
+
+// function cancelText(cancelBtn) {
+//     // 
+// }
+// cancel duzelt
+// function saveTask(saveBtn) {
+//     let saveItem = saveBtn.closest('li')
+//     let saveChanges = saveItem.querySelector('input').value
+//     saveItem.innerHTML=
+//     `<li>
+//         <div class="mx-auto border-b border-gray-300 p-3 flex justify-between">
+//             <span>${saveChanges}</span>
+//             <div>
+//                 <button onclick =editTask(this)  type="submit" class="text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 ">EDIT</button>
+//                 <button onclick = removeTask(this) type="submit" class="text-white bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 ">DELETE</button>
+//             </div>
+//         </div>
+//     </li>`
+// }
+
+
+
+        // VALIDASIYA
