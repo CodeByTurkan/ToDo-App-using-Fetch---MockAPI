d2e5c7419a3cf761bf7fa68a0533096021ed9e41
CodeByTurkan <turkantech@gmail.com>
2025-04-11 09:03:36 +0400

    bg change/ fixing bugs and null problem

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ function showList(data) {
             <div class="mx-auto border-b border-gray-300 p-3 flex justify-between">
                 <span>${item.text}</span>
                 <div>
-                    <button onclick=editTask(${item.id}, '${item.text}')  type="submit" class="text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 ">EDIT</button>
-                    <button onclick = removeTask(${item.id}) type="submit" class="text-white bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 ">DELETE</button>
+                    <button onclick = "editTaskFromButton(this)" data-id="${item.id}" data-text="${item.text}" type="submit" class="text-white bg-blue-600 font-medium rounded-lg text-sm px-4 py-2 ">EDIT</button>
+                    <button onclick = "removeTask(${item.id})" type="submit" class="text-white bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 ">DELETE</button>
                 </div>
             </div>
         </li>
@@ -44,8 +44,18 @@ function showList(data) {
     list.innerHTML = code
 }
 
+function editTaskFromButton(firstEdit) {
+    const id = firstEdit.getAttribute('data-id')
+    const text = firstEdit.getAttribute('data-text')
+    editTask(id, text);
+   
+    
+}
+
 // my logical hottakes
 // goruntunu bir defe burda yazirsan mentiq olaraq, ve sonrada elave et sil ve edit eti yazirsan.
+// possible errors : "" - quotes after the onclick = / if there is it's - you should give me data-id and write inside first diff functiona nd the add your func ther
+// if there isnull it means your value is empty so find why it is returning empty 
 
 function addTask(){
     if (text.value != '') {
@@ -72,6 +82,7 @@ function addTask(){
 
 function removeTask(id) {
     fetch(`${baseURL}/${id}`,{
+        // /todo/5 - -mockapi code icindeki nmberdi
         method:'DELETE',
         headers:{
             'content-type': 'application/json'
@@ -86,6 +97,9 @@ function removeTask(id) {
 }
 function editTask(id, text) {
     let pr = prompt('Please, add value to Update!', text)
+    // prompt(message, defaultValue)
+    // let userInput = prompt("What's your name?", "Turkan")
+
     if (pr != '' && pr != null) {
         fetch(`${baseURL}/${id}`,{
          method: 'PUT',
@@ -95,7 +109,8 @@ function editTask(id, text) {
          body: JSON.stringify({text: pr})
          
         })
-        .then(res =>{res.json()
+        .then(res =>{
+        res.json()
             if (res.ok) {
                 getList()
             } 
@@ -104,5 +119,3 @@ function editTask(id, text) {
         
     }
 }
-
-        // VALIDASIYA
